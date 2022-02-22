e9751e813be6611c2942eb9c98929d04ed0caf4a
Romuald Thion <romulusFR@users.noreply.github.com>
2022-02-23 10:09:54 +1100

    feat: monitorEventLoopDelay

--- demos/starter/signal.mjs
+++ demos/starter/signal.mjs
@@ -1,6 +1,11 @@
 /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
 import process from "node:process";
 import { setInterval } from "node:timers/promises";
+import { monitorEventLoopDelay } from "node:perf_hooks";
+
+// pour jouer avec https://nodejs.org/api/perf_hooks.html#perf_hooks_perf_hooks_monitoreventloopdelay_options
+const h = monitorEventLoopDelay({ resolution: 20 });
+h.enable();
 
 // We are using this single function to handle multiple signals
 function handle(signal) {
@@ -24,8 +29,22 @@ process.on("exit", (code) => {
 
 console.log("This message is displayed first.");
 
-const cancelTask = new AbortController();
+const doSomeSyncWork = (delay) => {
+  const s = Date.now();
+  while (true) {
+    const array = Array.from({ length: 1000 });
+    array.fill("hello");
+    if (Date.now() - s > delay) {
+      break;
+    }
+  }
+};
 
+console.log("Starting sync job.");
+doSomeSyncWork(3000);
+console.log("Sync job done.");
+
+const cancelTask = new AbortController();
 
 const interval = 1000;
 try {
@@ -37,5 +56,15 @@ try {
 } catch {
   console.log("Task graciously finished.");
 }
+
+h.disable();
+const pretty = (f) => (f / 10e6).toFixed(3);
+console.debug(`min  = ${pretty(h.min)} ms`);
+console.debug(`max  = ${pretty(h.max)} ms`);
+console.debug(`mean = ${pretty(h.mean)} ms`);
+console.debug(`dev  = ${pretty(h.stddev)} ms`);
+// console.debug(pretty(h.percentile(50)));
+// console.debug(pretty(h.percentile(99)));
+// console.log(h.percentiles);
 
 console.log("This message is displayed last.");
