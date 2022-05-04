fbbd53146b02e5b3ecb089f66e9edebfd2c108b5
The_Phoenix78 <julien.lcq@icloud.com>
2022-05-04 11:10:34 +1100

    maj utilities folder

--- src/utilities/downloadEdt.mjs
+++ src/utilities/downloadEdt.mjs
@@ -1,62 +1,50 @@
-/*
-var moment = require('moment');
-//const { DownloaderHelper } = require('node-downloader-helper');
-
-const fs = require("fs");
-const request = require("request-promise-native");
-*/
 import moment from "moment";
 import fs from "fs";
-import request from "request-promise-native"
+import fetch from "node-fetch";
 
 
 async function downloadPDF(pdfURL, outputFilename) {
-    let pdfBuffer = await request.get({uri: pdfURL, encoding: null});
-    // console.log("Writing downloaded PDF file to " + outputFilename + "...");
-    fs.writeFileSync(outputFilename, pdfBuffer);
+    let resp = await fetch(pdfURL);
+    // if (!resp.ok) throw new Error("Unexpected response");
+    let data = await resp.buffer();
+    /*let data = await resp.arrayBuffer();*/
+
+    fs.writeFileSync(outputFilename, data);
 }
 
 
-function downloadEdt(pdf_name, indices = null, plus = 0) {
-  var current_date, num_semaine, path_to_pdf, url, url_edt;
+function downloadEdt(pdf_name, chiffres_magique, plus = 0) {
+  let current_date, num_semaine, path_to_pdf, url, url_edt;
 
-  var ts = Date.now();
+  //let ts = Date.now();
 
-  var date_time = new Date(ts);
+  let date_time = new Date();
 
-  var day = date_time.getDate();
-  var month = date_time.getMonth() + 1;
-  var year = date_time.getFullYear();
+  let day = date_time.getDate();
+  let month = date_time.getMonth() + 1;
+  let year = date_time.getFullYear();
 
-  var today = day + "-" + month + "-" + year
+  current_date = moment(day+"-"+month+"-"+year, "DD-MM-YYYY");
 
-
-  var  current_date = moment(today, "DD-MM-YYYY");
-
-  var num_semaine = current_date.week()-1;
-  var num_day = current_date.day();
+  num_semaine = current_date.week()-1;
+  let num_day = current_date.day();
 
   if (num_day > 5) {
     num_semaine += 1;
   }
 
-  while (num_semaine - indices[2] < 0) {
+  while (num_semaine - chiffres_magique[2] < 0) {
     num_semaine += 1;
   }
 
-  var ch_magique = num_semaine - indices[2] + plus;
+  let ch_magique = num_semaine - chiffres_magique[2] + plus;
 
-  url = "http://applis.univ-nc.nc/gedfs/edtweb2/" + indices[0] + "." + (ch_magique) + "/PDF_EDT_" + indices[1] + "_"+ (num_semaine + plus) +"_"+ year +".pdf";
+  url = "http://applis.univ-nc.nc/gedfs/edtweb2/" + chiffres_magique[0] + "." + (ch_magique) + "/PDF_EDT_" + chiffres_magique[1] + "_"+ (num_semaine + plus) +"_"+ year +".pdf";
   path_to_pdf = "edt/"+pdf_name;
 
-  /*
-  const download = new DownloaderHelper(url, __dirname);
-  download.on('end', () => console.log('Download Completed'))
-  download.start();
-  */
   downloadPDF(url, pdf_name);
 
   return url;
 }
 
-// console.log(downloadEdt("test.pdf", [2201271126, 15404, 9]))
+console.log(downloadEdt("test.pdf", [2201271126, 15404, 9]))
---
+++ src/utilities/getHeader.mjs
@@ -0,0 +1,36 @@
+import moment from "moment";
+
+
+function getHeader(chiffres_magique, plus=0){
+    let current_date, num_semaine, url, url_edt;
+
+    //let ts = Date.now();
+
+    let date_time = new Date();
+
+    let day = date_time.getDate();
+    let month = date_time.getMonth() + 1;
+    let year = date_time.getFullYear();
+
+    current_date = moment(day+"-"+month+"-"+year, "DD-MM-YYYY");
+
+    num_semaine = current_date.week()-1;
+    let num_day = current_date.day();
+
+    if (num_day > 5) {
+    num_semaine += 1;
+    }
+
+    while (num_semaine - chiffres_magique[2] < 0) {
+    num_semaine += 1;
+    }
+
+    let ch_magique = num_semaine - chiffres_magique[2] + plus;
+
+    url = "http://applis.univ-nc.nc/gedfs/edtweb2/" + chiffres_magique[0] + "." + (ch_magique) + "/PDF_EDT_" + chiffres_magique[1] + "_"+ (num_semaine + plus) +"_"+ year +".pdf";
+
+
+    return ;
+}
+
+// console.log(getHeader([ '2202041315', '16482', 14 ]));
